13c149bad63853dfcc70f729008f1d2a6780ef98
Stéphane GUILLY <stephaneguilly@MacBook-Pro-de-Stephane.local>
2021-06-07 17:30:57 +0200

    feat(server): log taskerror

--- src/server.js
+++ src/server.js
@@ -50,6 +50,14 @@ const swaggerSpec = swaggerJSDoc(options);
         },
     });
 
+    cluster.on('taskerror', (err, data, willRetry) => {
+        if (willRetry) {
+          console.warn(`Encountered an error while crawling ${data}. ${err.message}\nThis job will be retried`);
+        } else {
+          console.error(`Failed to crawl ${data}: ${err.message}`);
+        }
+    });
+
     const generate = (params) => {
 
         const task = async ({ page, data }) => {
